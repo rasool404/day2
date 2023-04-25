50c2df8285ddf0e7ca39c0be10f9608628195a76
Rasul <test@gmail.com>
2023-04-25 19:05:07 +0500

    adding function that returns the result for the entire sudoku board

--- 2.js
+++ 2.js
@@ -9,3 +9,20 @@ const board = [
   [".", ".", ".", "4", "1", "9", ".", ".", "5"],
   [".", ".", ".", ".", "8", ".", ".", "7", "9"],
 ];
+// A function that returns the result for the entire sudoku board.
+function validSudoku(board) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      const value = board[i][j];
+      if (value !== ".") {
+        if (
+          !validRow(board, i, j, value) ||
+          !validColumn(board, i, j, value) | !validBox(board, i, j, value)
+        ) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
