f942062222b92b1eca3357b0bb8d31c475d0d5e4
Rasul <test@gmail.com>
2023-04-25 19:18:32 +0500

    add the sub-boxes function

--- 2.js
+++ 2.js
@@ -11,29 +11,39 @@ const board = [
 ];
 
 function validRow(board, row, col, value) {
-  // j represents on column
   for (let j = 0; j < 8; j++) {
-    // check if the current column matches the passed in column
     if (j !== col) {
       if (board[row][j] === value) {
         return false;
       }
     }
   }
-
   return true;
 }
 
 function validColumn(board, row, col, value) {
-  // j represents on row
   for (let i = 0; i < 8; i++) {
-    // check if the current row matches the passed in row
     if (i !== row) {
       if (board[i][col] === value) {
         return false;
       }
     }
   }
+  return true;
+}
 
+function validBox(board, row, col, value) {
+  const startRow = row - (row % 3),
+    startCol = col - (col % 3);
+
+  for (let i = startRow; i < startRow + 3; i++) {
+    for (let j = startCol; j < startCol + 3; j++) {
+      if (i !== row && j !== col) {
+        if (board[i][j] === value) {
+          return false;
+        }
+      }
+    }
+  }
   return true;
 }
