3be02229a32cd3851a56f65dfc6500c636c2e5f2
Rasul <test@gmail.com>
2023-04-25 19:17:20 +0500

    refactor functions

--- 2.js
+++ 2.js
@@ -10,25 +10,10 @@ const board = [
   [".", ".", ".", ".", "8", ".", ".", "7", "9"],
 ];
 
-const validSudoku = (board) => {
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      const value = board[i][j];
-      if (value !== ".") {
-        if (
-          !validRow(board, i, j, value) ||
-          !validColumn(board, i, j, value) | !validBox(board, i, j, value)
-        ) {
-          return false;
-        }
-      }
-    }
-  }
-  return true;
-};
-
-const validRow = (board, row, col, value) => {
+function validRow(board, row, col, value) {
+  // j represents on column
   for (let j = 0; j < 8; j++) {
+    // check if the current column matches the passed in column
     if (j !== col) {
       if (board[row][j] === value) {
         return false;
@@ -37,4 +22,18 @@ const validRow = (board, row, col, value) => {
   }
 
   return true;
-};
+}
+
+function validColumn(board, row, col, value) {
+  // j represents on row
+  for (let i = 0; i < 8; i++) {
+    // check if the current row matches the passed in row
+    if (i !== row) {
+      if (board[i][col] === value) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
